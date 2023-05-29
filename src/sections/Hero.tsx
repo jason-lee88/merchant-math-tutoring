f889149fc5bf3064d978ebc51b76190ac3a12e05
Schmear-Merch99 <srmerchant99@gmail.com>
2023-05-28 21:06:29 -0400

    change philly area

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -92,7 +92,7 @@ const Hero = () => (
     <div id="hero-content">
       <h1>Get Started Today</h1>
       <h2>Mathematics, Electricity, & More</h2>
-      <h3>Bucks County, PA</h3>
+      <h3>Greater Philadelphia Area</h3>
       <a href="#pricing">View Pricing</a>
     </div>
   </StyledSection>

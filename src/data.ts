620acb0c381541f8677f6d3d365002e92379360d
Schmear-Merch99 <srmerchant99@gmail.com>
2025-04-02 18:27:26 -0400

    ADD NEW TUTOR

--- src/data.ts
+++ src/data.ts
@@ -1,15 +1,16 @@
-import EricLai from "./assets/tutors/eric-lai.jpg";
-import MadelineBraun from "./assets/tutors/madeline-braun.png";
-import SophiaMartino from "./assets/tutors/sophia-martino.jpg";
-import NolanJetter from "./assets/tutors/nolan-jetter.jpg";
+//import EricLai from "./assets/tutors/eric-lai.jpg";
+//import MadelineBraun from "./assets/tutors/madeline-braun.png";
+//import SophiaMartino from "./assets/tutors/sophia-martino.jpg";
+//import NolanJetter from "./assets/tutors/nolan-jetter.jpg";
 import JoshuaHall from "./assets/tutors/joshua-hall.jpg";
 import QuanHoang from "./assets/tutors/quan-hoang.jpg";
-import EthanFisherPerez from "./assets/tutors/ethan-fisher-perez.jpg";
-import AndersHeyniger from "./assets/tutors/anders-heyniger.png";
+//import EthanFisherPerez from "./assets/tutors/ethan-fisher-perez.jpg";
+//import AndersHeyniger from "./assets/tutors/anders-heyniger.png";
 import MeganFreid from "./assets/tutors/megan-freid.png";
-import AriannaMorataya from "./assets/tutors/arianna-morataya.jpg";
-import EmmaAnderson from "./assets/tutors/emma-anderson.jpg";
-import LukeRekucki from "./assets/tutors/luke-rekucki.jpg";
+//import AriannaMorataya from "./assets/tutors/arianna-morataya.jpg";
+//import EmmaAnderson from "./assets/tutors/emma-anderson.jpg";
+//import LukeRekucki from "./assets/tutors/luke-rekucki.jpg";
+import ImonRahaman from "./assets/tutors/imon-rahaman.jpg";
 
 interface Data {
   tabs: {
@@ -100,19 +101,19 @@ const Data: Data = {
   pricing: [
     {
       label: "Individual",
-      cost: 75,
+      cost: 100,
     },
     {
       label: "Group (2 Students)",
-      cost: 55,
+      cost: 80,
     },
     {
       label: "Group (3 Students)",
-      cost: 45,
+      cost: 60,
     },
     {
       label: "Group (4 Students)",
-      cost: 40,
+      cost: 55,
     },
   ],
   testimonials: [
@@ -164,54 +165,6 @@ const Data: Data = {
   ],
   tutors: [
     {
-      name: "Eric Lai",
-      imagePath: EricLai,
-      imagePosition: [0, 150],
-      imagePositionMobile: [140, 210],
-      imageScaling: 500,
-      imageScalingMobile: 500,
-      bio: [
-        "I’m Eric Lai, currently a senior pursuing a B.S. in Chemical Engineering at Lehigh University. Some information about me: I am the president of the Lehigh University eSports club, I have two black belts and I’m a big foodie! I believe there is nothing more frustrating in academics than a professor that knows practically everything in his discipline and can’t relay even a fraction of that knowledge to his/her students. I can’t recall how many times I skipped an entire semester’s worth of classes because I found it more beneficial to learn through the textbook than through lecture-based instruction. I am interested in tutoring because I believe that your time is valuable and shouldn’t be wasted through inefficient means of learning. Learning the course material is something you should expect from any tutor, but I also want to help you develop strong study habits that will guide you to prolonged success in college.",
-        "Eric tutors chemistry, calculus 1, material balances, fluid mechanics, thermodynamics, methods, pchem, reactor design, and biology.",
-      ],
-    },
-    {
-      name: "Madeline Braun",
-      imagePath: MadelineBraun,
-      imagePosition: [155, 300],
-      imagePositionMobile: [270, 400],
-      imageScaling: 800,
-      imageScalingMobile: 800,
-      bio: [
-        "My name is Madeline Braun and I am a third year Mechanical Engineering student at Lehigh! As a new tutor at Merchant, I look forward to forming connections with other students who are working to learn the course content as I once was. Aside from being a student, I am an RA for sophomore housing and work at the Goose deli! I enjoy listening to music and watching Queen’s Gambit. Recognizing that some courses at Lehigh require extra attention, I am excited to be a source of help for others in their learning process.",
-        "Madeline tutors calculus 1, physics 1, statics, dynamics, and strength of materials.",
-      ],
-    },
-    {
-      name: "Sophia Martino",
-      imagePath: SophiaMartino,
-      imagePosition: [0, 300],
-      imagePositionMobile: [10, 260],
-      imageScaling: 800,
-      imageScalingMobile: 600,
-      bio: [
-        "Hi! I am Sophia Martino and I am studying computer engineering with a minor in business and a minor in data science. I also participate in acapella on campus; I am the music director of the Lehigh Echoes. I grew up in Pennsylvania outside of Philly and my family is from Paraguay! Lastly, I enjoy following Formula 1 and playing chess if I’ve got the time.",
-        "Sophia tutors calculus and electrical engineering subjects that include but aren't limited to circuits, signals and systems, and microelectronics.",
-      ],
-    },
-    {
-      name: "Nolan Jetter",
-      imagePath: NolanJetter,
-      imagePosition: [170, 0],
-      imagePositionMobile: [170, 20],
-      imageScaling: 800,
-      imageScalingMobile: 600,
-      bio: [
-        "I’m a junior here at lehigh doing a dual degree in mechanical engineering and biology. I got into engineering when I was young—I liked that I could play with numbers and objects and make them do what I wanted for me. I have only grown to enjoy this more as I have gotten older. I like to do stuff outside of academics as well - I play on the men’s soccer team here at Lehigh and enjoy spending time outside (biking, surfing, and skiing), reading, playing piano, and playing chess. Tutoring is really attractive to me because I believe that the surest sign of deep knowledge in a field is the ability to explain concepts as simply and intuitively as possible. I think that it’s a great opportunity to pass on my knowledge in clear, concise terms and sharpen my skills as a byproduct",
-        "Nolan tutors statics, dynamics, strength of materials, thermodynamics, and other mechanical engineering subjects.",
-      ],
-    },
-    {
       name: "Joshua Hall",
       imagePath: JoshuaHall,
       imagePosition: [40, 20],
@@ -219,22 +172,11 @@ const Data: Data = {
       imageScaling: 600,
       imageScalingMobile: 500,
       bio: [
-        "My name is Josh Hall, and I'm a first year undergraduate student at Lehigh. I'm duel majoring in CS and Mathematics, and I've also been a musician for 10 years! I've found that professors sometimes have a habit of skipping steps they deem 'irrelevant' or 'easy', and sometimes I get lost with the omitted details. As a tutor, my main method is to cover these intermediate steps, or study the more niche problems in a subject. I also try to give helpful tips and shortcuts to problems. I've always enjoyed helping my peers in their classes with anything they have been struggling to understand. I love tutoring because I get invested in those I'm teaching, and I love to see them succeed!",
+        "My name is Josh Hall, and I'm a junior undergraduate student at Lehigh. I'm duel majoring in CS and Mathematics, and I've also been a musician for 10 years! I've found that professors sometimes have a habit of skipping steps they deem 'irrelevant' or 'easy', and sometimes I get lost with the omitted details. As a tutor, my main method is to cover these intermediate steps, or study the more niche problems in a subject. I also try to give helpful tips and shortcuts to problems. I've always enjoyed helping my peers in their classes with anything they have been struggling to understand. I love tutoring because I get invested in those I'm teaching, and I love to see them succeed!",
         "Joshua tutors calculus 1, calculus 2, and calculus 3.",
       ],
     },
-    {
-      name: "Emma Anderson",
-      imagePath: EmmaAnderson,
-      imagePosition: [70, 70],
-      imagePositionMobile: [60, 60],
-      imageScaling: 600,
-      imageScalingMobile: 400,
-      bio: [
-        "My name is Emma Anderson and am a senior at Lehigh University earning a degree in bioengineering with a focus in Pharmaceuticals and a minor in Economics. I am the academic chair in my sorority and enjoy helping other students succeed academically. I am thrilled to be able to help even more students in their academics. In my free time I love to bake gluten free treats, enjoy going to the gym, and rollerblading.",
-        "Emma tutors Economics and Calculus 1."
-      ],
-    },
+   
     {
       name: "Quan Hoang",
       imagePath: QuanHoang,
@@ -243,35 +185,11 @@ const Data: Data = {
       imageScaling: 600,
       imageScalingMobile: 400,
       bio: [
-        "Hello! My name is Quan Hoang and I am currently a third-year student majoring in both Chemical Engineering and Biochemistry. I am involved in many of the musical ensembles here at Lehigh, as well as many STEM related organizations. In the past, I have worked as a Chemistry tutor at Lehigh, and I hope to continue tutoring students with Merchant. I understand that it could be frustrating and difficult to seek assistance in classes, and I hope that I could be a resource to help those students who need it!",
+        "Hello! My name is Quan Hoang and I am currently a fourth-year student majoring in both Chemical Engineering and Biochemistry. I am involved in many of the musical ensembles here at Lehigh, as well as many STEM related organizations. In the past, I have worked as a Chemistry tutor at Lehigh, and I hope to continue tutoring students with Merchant. I understand that it could be frustrating and difficult to seek assistance in classes, and I hope that I could be a resource to help those students who need it!",
         "Quan tutors chemistry, material balances, fluid mechanics, thermodynamics, methods, pchem, and reactor design.",
       ],
     },
-    {
-      name: "Ethan Fisher-Perez",
-      imagePath: EthanFisherPerez,
-      imagePosition: [70, 30],
-      imagePositionMobile: [60, 20],
-      imageScaling: 600,
-      imageScalingMobile: 400,
-      bio: [
-        "My name is Ethan Fisher-Perez. I’m a Lehigh senior studying Behavioral Neuroscience and Sociology. I’ve been doing research as a member of the Miwa Lab since my first year, where we study proteins involved in neuroplasticity. I’m scientifically most interested in learning, mood disorders, and smell.  I’m a writer, and I spent 5 semesters as a member of the Brown & White, where I was an editor, reported for the Health and Science beat, and wrote a column (mostly) about neuroscience called Gray Matters. I hope to soon publish the first issue of an online magazine that publishes psychological pieces of writing called Above Cognition.",
-        "I’m from a rural town an hour north of Bethlehem called Kunkletown. It is there I discovered my interest in lifting as well as coffee brewing and cupping. I love all kinds of art, but I am particularly devoted to the Renaissance and French impressionism. I love  reading, thinking, and handwriting—now that I think about it, most probably in that order.",
-        "Ethan tutors biology, physics 1, and physics 2.",
-      ],
-    },
-    {
-      name: "Anders Heyniger",
-      imagePath: AndersHeyniger,
-      imagePosition: [40, 100],
-      imagePositionMobile: [50, 70],
-      imageScaling: 600,
-      imageScalingMobile: 400,
-      bio: [
-        "What’s up! I’m Anders, a Computer Science student at Lehigh University’s P.C. Rossin School of Engineering and Applied Sciences. I love playing video games, watching shows and movies, and just really anything to do with computers. I also love to play and learn about music! I first realized my love of coding in 10th grade when I took my first computer science class and just couldn’t stop learning about it, and as a result I often ended up helping friends and those around me who were struggling with coding. I’ve been practically teaching coding to people frequently ever since then, and over time I’ve realized how much I enjoy helping people learn it too! I always strive to find what others might not understand about it (as computer science can be daunting, I know) and I hope to use the ways I’ve learned to approach programming to help other people be able to understand it and see it in a much easier and more interesting light!",
-        "Anders tutors computer science.",
-      ],
-    },
+ 
     {
       name: "Megan Freid",
       imagePath: MeganFreid,
@@ -285,29 +203,22 @@ const Data: Data = {
       ],
     },
     {
-      name: "Arianna Morataya",
-      imagePath: AriannaMorataya,
-      imagePosition: [40, 200],
-      imagePositionMobile: [50, 150],
+      name: "Imon Rahaman",
+      imagePath: ImonRahaman,
+      imagePosition: [40, 70],
+      imagePositionMobile: [50, 40],
       imageScaling: 600,
       imageScalingMobile: 400,
       bio: [
-        "My name is Arianna Morataya and I am a third year Accounting student with a Minor in Spanish. I am also pursuing an Undergraduate International Business Certificate through the College of Business that would accompany my B.S degree. I am from New Jersey but went to boarding school in New Hampshire so I have experienced all types of places. I am in several clubs at Lehigh that include Women of Color Alliance, Lehigh Minorities in Business Alliance, and Women in Business! I also work for the America Reads/Counts Organization that sponsors after-school homework programs for elementary students in the Bethlehem area. As a new tutor, I look forward to assisting students in their accounting courses and helping them succeed overall! ",
-        "Arianna tutors finance and accounting."
+        "Hi, my name is Imon Rahaman. I am a clinical researcher working in the genetics department at the Children’s Hospital of Philadelphia. I graduated from MIT (class of 2020) with a degree in biological engineering and am currently pursuing a Master of Science in Bioinformatics at Johns Hopkins University (expected 2025). With over three years of experience in academic research, I specialize in biology laboratory techniques, computational analysis, and data science, particularly within the fields of biotechnology and medicine. I am passionate about sharing my knowledge and helping others grasp complex topics, which is why I offer tutoring services in all STEM subjects and specifically upper level biology, mathematics, computer science, and related subjects.", 
+
+        "As a former premed student, I am also able to offer guidance on building a strong application when applying for medical school, especially in succeeding on the MCAT on which I scored a 517 (94th percentile).", 
+        
+        "I look forward to helping students learn and succeed on their academic journey!"
+
       ],
     },
-    {
-      name: "Luke Rekucki",
-      imagePath: LukeRekucki,
-      imagePosition: [70, 30],
-      imagePositionMobile: [50, 0],
-      imageScaling: 600,
-      imageScalingMobile: 400,
-      bio: [
-        "My name is Luke Rekucki, and I am going into my final semester at Lehigh. I am majoring in Mechanical Engineering, and will be receiving both a business and engineering degree through the Integrated Business and Engineering program. I was born and raised in northern New Jersey, where I am currently interning at Becton Dickinson as an R&D engineer in new product development. Outside of school, I love to play video games and watch movies on a rainy day, and take advantage of the sunshine by getting active with hikes or playing sports with friends. My favorite part of tutoring is learning from my students, especially about how great their exam grades are!",
-        "Luke tutors Mechanical engineering subjects and Statistics."
-      ],
-    },
+
   ],
   subjects: ["Engineering", "Calculus", "Statistics", "Algebra", "Geometry", "Biology", "Chemistry", "Physics", "Accounting", "Economics", "Computer Science"],
 };
